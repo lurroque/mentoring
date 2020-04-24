0b4b1379acbc431edbdb81b330aa2100107c9126
Roque <lrvroque@gmail.com>
2020-04-24 11:43:32 -0300

    applaying pep8

--- tests/test_oo.py
+++ tests/test_oo.py
@@ -8,15 +8,16 @@ def test_create_film():
     assert film.year == 1986
     assert film.duration == 160
 
+
 def test_add_likes_for_film():
     film = Movie("the lord of the rings", 1986, 160)
     film.add_like()
 
     assert film.likes == 1
 
+
 def test_add_likes_for_film_when_is_not_zero():
     film = Movie("the lord of the rings", 1986, 160)
     film.add_like()
 
     assert film.likes != 0
-
