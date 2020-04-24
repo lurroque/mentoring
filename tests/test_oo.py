3533c5a66b625021a171368f94d3531d6ee1b985
Roque <lrvroque@gmail.com>
2020-04-23 21:51:20 -0300

    testing add_like method

--- tests/test_oo.py
+++ tests/test_oo.py
@@ -2,8 +2,14 @@ from oo.models.models import Film
 
 
 def test_create_film():
-
     film = Film("the lord of the rings", 1986, 160)
+
     assert film.name == "the lord of the rings".title()
     assert film.year == 1986
     assert film.duration == 160
+
+def test_add_likes_for_film():
+    film = Film("the lord of the rings", 1986, 160)
+    film.add_like()
+
+    assert film.likes == 1
